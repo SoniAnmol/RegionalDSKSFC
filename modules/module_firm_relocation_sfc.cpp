fddf291f51e4d62bb97e289170b63693118ddfe0
SoniAnmol <anmol_soni@outlook.com>
2026-08-25 14:17:38 +0200

    firm mobility: added relocation module skeleton

---
+++ modules/module_firm_relocation_sfc.cpp
@@ -0,0 +1,7 @@
+#include "module_firm_relocation_sfc.h"
+
+void FIRM_RELOCATION_COMPUTATION(void)
+{
+    // Intentionally empty.
+    // Firm relocation behaviour will be added incrementally.
+}
---
+++ modules/module_firm_relocation_sfc.h
@@ -0,0 +1,7 @@
+#ifndef MODULE_FIRM_RELOCATION_SFC_H
+#define MODULE_FIRM_RELOCATION_SFC_H
+
+// Firm regional relocation
+void FIRM_RELOCATION_COMPUTATION(void);
+
+#endif
